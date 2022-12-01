bf2d77bdd02793e577ae851e867210dfbc01944a
leoggehrer <g.gehrer@htl-leonding.ac.at>
2022-12-01 15:51:14 +0100

    Add project files.

---
+++ Repository.Logic/Repos/AlbumRepository.cs
@@ -0,0 +1,17 @@
+﻿using Repository.Logic.Models;
+
+namespace Repository.Logic.Repos
+{
+    public class AlbumRepository : Repository<Models.Album>
+    {
+        public AlbumRepository()
+            : base()
+        {
+
+        }
+        public override Album Create()
+        {
+            return new Album();
+        }
+    }
+}
